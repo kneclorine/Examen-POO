c08bdaabc76d4919a607f45ed74c9d73337eae87
Alexander García <alexander.garcia-mas@capgemini.com>
2021-08-03 08:03:26 +0200

    Corrección 0.3

--- src/App.java
+++ src/App.java
@@ -1,17 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Canvas lienzo = new Canvas();
+        Canvas canvas = new Canvas();
 
-        Shape figura1 = Service.createShape(Shapes.HEART);
-        lienzo.addShape(figura1, new Rectangle(new Point(0, 0) , new Point(10, 20)));
+        Shape shape1 = Service.createShape(Shapes.HEART);
+        canvas.addShape(shape1, new Rectangle(new Point(0, 0) , new Point(10, 20)));
 
-        Shape figura2 = Service.createShape(Shapes.LIGHTNING);
-        lienzo.addShape(figura2, new Rectangle(new Point(0, 25) , new Point(10, 30)));
+        Shape shape2 = Service.createShape(Shapes.LIGHTNING);
+        canvas.addShape(shape2, new Rectangle(new Point(0, 25) , new Point(10, 30)));
         
 
         Point selection = new Point(0, 0); // Punto donde hacemos click para seleccionar una figura.
-        Shape selectedShape = lienzo.getSelected(selection); //Figura que se encuentre en ese punto.
+        Shape selectedShape = canvas.getSelected(selection); //Figura que se encuentre en ese punto.
 
         if(selectedShape != null){
             selectedShape.setBorderColor("negro");
@@ -20,7 +20,7 @@ public class App {
         }
         
         selection = new Point(7, 28);
-        selectedShape = lienzo.getSelected(selection); 
+        selectedShape = canvas.getSelected(selection); 
         
         if(selectedShape != null){
             selectedShape.setBorderColor("azul");
